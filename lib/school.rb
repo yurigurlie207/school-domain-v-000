11c0a20022079667f48ede62753ce40f22204b5e
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-29 20:34:39 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,6 +14,7 @@ class School
   def add_student(student, grade)
     if @roster[grade] == nil
       @roster[grade] = []
+    end
       @roster[grade] << student
     else
       @roster[grade] << student
